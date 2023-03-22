1843a8bd40baba50b23069ed738406f431848d18
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-03-23 00:43:48 +0530

    Add files via upload

---
+++ CheckEven.java
@@ -0,0 +1,16 @@
+public class CheckEven{
+    void checkEven(int n){
+     if (n%2==0){
+        System.out.println(n);
+     }
+    }
+    public static void main(String[] args) {
+        CheckEven a=new CheckEven();
+        int n=5;
+        a.checkEven(n);
+        System.out.println(n);
+        
+    }
+}
+    
+
